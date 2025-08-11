3654b04ecef3297d1ee1f1b82888b6bcfc1e2e32
Luna Razzaghipour <luna@xoria.org>
2025-08-11 10:40:53 +1000

    Customize popover animation

--- CustomPresentations/CustomPopoverExampleViewController.swift
+++ CustomPresentations/CustomPopoverExampleViewController.swift
@@ -23,6 +23,18 @@ final class CustomPopoverExampleViewController: UIViewController {
 private final class CustomPopoverTransitioningDelegate: NSObject, UIViewControllerTransitioningDelegate {
 	static let shared = CustomPopoverTransitioningDelegate()
 
+	func animationController(
+		forPresented _: UIViewController,
+		presenting _: UIViewController,
+		source _: UIViewController,
+	) -> (any UIViewControllerAnimatedTransitioning)? {
+		CustomPopoverAnimationController(type: .presentation)
+	}
+
+	func animationController(forDismissed _: UIViewController) -> (any UIViewControllerAnimatedTransitioning)? {
+		CustomPopoverAnimationController(type: .dismissal)
+	}
+
 	func presentationController(
 		forPresented presented: UIViewController,
 		presenting: UIViewController?,
@@ -40,5 +52,66 @@ private final class CustomPopoverPresentationController: UIPresentationControlle
 		origin.x -= size.width / 2
 		origin.y += 50
 		return CGRect(origin: origin, size: size)
+	}
+}
+
+private final class CustomPopoverAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+	private let type: PresentationType
+
+	init(type: PresentationType) {
+		self.type = type
+	}
+
+	func transitionDuration(using _: (any UIViewControllerContextTransitioning)?) -> TimeInterval {
+		type == .presentation ? 0.4 : 0.2
+	}
+
+	func animateTransition(using transitionContext: any UIViewControllerContextTransitioning) {
+		guard
+			let fromViewController = transitionContext.viewController(forKey: .from),
+			let toViewController = transitionContext.viewController(forKey: .to),
+			let fromView = fromViewController.view,
+			let toView = toViewController.view
+		else {
+			transitionContext.completeTransition(false)
+			return
+		}
+
+		let (presentedViewController, presentedView) = switch type {
+		case .presentation: (toViewController, toView)
+		case .dismissal: (fromViewController, fromView)
+		}
+
+		if type == .presentation {
+			let containerView = transitionContext.containerView
+			containerView.addSubview(presentedView)
+		}
+
+		let finalFrame = transitionContext.finalFrame(for: presentedViewController)
+		presentedView.frame = finalFrame
+
+		let presentedTransform = CGAffineTransform.identity
+		let dismissedTransform = CGAffineTransform(translationX: 0, y: -0.5 * finalFrame.height)
+			.scaledBy(x: 0.001, y: 0.001)
+
+		presentedView.transform = type == .presentation ? dismissedTransform : presentedTransform
+
+		let duration = transitionDuration(using: transitionContext)
+		let bounce: CGFloat = type == .presentation ? 0.2 : 0
+		let timingParameters = UISpringTimingParameters(duration: duration, bounce: bounce)
+		let animator = UIViewPropertyAnimator(duration: 0, timingParameters: timingParameters)
+
+		animator.addAnimations { [self] in
+			presentedView.transform = type == .presentation ? presentedTransform : dismissedTransform
+		}
+		animator.addCompletion { position in
+			transitionContext.completeTransition(position == .end)
+		}
+		animator.startAnimation()
+	}
+
+	enum PresentationType {
+		case presentation
+		case dismissal
 	}
 }
